49dd0041f4b17c53afb96eb89cd7cf7230a30f7b
Alejandro <alejandrolampreave@gmail.com>
2019-01-12 09:58:21 +0100

    Creada interfaz de subida de ficheros.

---
+++ Interface/upload_file.php
@@ -0,0 +1,16 @@
+<?php
+if ($_FILES['archivo']["error"] > 0)
+  {
+  echo "Error: " . $_FILES['archivo']['error'] . "<br>";
+  }
+else
+  {
+  echo "Nombre: " . $_FILES['archivo']['name'] . "<br>";
+  echo "Tipo: " . $_FILES['archivo']['type'] . "<br>";
+  echo "Tamaño: " . ($_FILES["archivo"]["size"] / 1024) . " kB<br>";
+  echo "Carpeta temporal: " . $_FILES['archivo']['tmp_name'];
+ 
+  
+
+}
+?>
