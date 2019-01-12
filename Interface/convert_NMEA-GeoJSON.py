fcb2ad550ead6c9a139deccef7f6053b904bd125
Alejandro <alejandrolampreave@gmail.com>
2019-01-12 11:38:45 +0100

    Añadidos comentarios en convert_NMEA_GeoJSON

--- Interface/convert_NMEA-GeoJSON.py
+++ Interface/convert_NMEA-GeoJSON.py
@@ -1,13 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 # coding: utf-8
+# In[76]:
+'''
+Script de Python que se encarga de la conversión de los mensajes NMEA recogidos
+por la placa de desarrollo FDRMK64F al estándar GeoJSON necesario para la 
+interpretación de la API de mapbox que muestra los mapas.
+@author: Alejandro Fernández Lampreave
+'''
 
-# In[71]:
-
-
+import sys ##
 import requests
 import re
+miarchivo = sys.argv[1] ##
 url = 'https://mygeodata.cloud/api/convert'
-files = {'file': open('LOG_GPS_NMEA.txt', 'rb')}
+files = {'file': open(miarchivo, 'rb')} 
 data = {'format': 'geojson', 'outcrs': 'EPSG:4326', 'outform': 'binary', 'key': '5PsoGucmRRD2h0c'}
 r = requests.post(url, files=files, data=data)
 d = r.headers['content-disposition']
@@ -15,4 +21,10 @@ fname = re.findall("filename=(.+)", d)[0]
 with open(fname, 'wb') as fd:
     for chunk in r.iter_content(1000):
         fd.write(chunk)
+
+
+# In[ ]:
+
+
+
 
